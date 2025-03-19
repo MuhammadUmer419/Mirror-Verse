bf78362d5a04af064c64615f8a19d60633a02ec2
MuhammadUmer419 <whoisumree@gmail.com>
2025-03-19 04:11:19 -0700

    Updated HTML & CSS files

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ $(document).ready(function(){
         margin: 20,
         nav: true,
         autoplay:false,
-        autoplayTimeout: 3000,
         autoplayHoverPause: true,
         responsive:{
             0:{ items: 1 },
